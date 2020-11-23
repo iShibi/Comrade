81244e9babdbf94a795120612d8db6689c81c99c
Shubham Parihar <shubhamparihar391@gmail.com>
2020-11-23 15:12:33 +0530

    fixed: clear cmd won't try to delete messages older than two weeks now

--- commands/clear.js
+++ commands/clear.js
@@ -5,14 +5,16 @@ module.exports = {
         const msgDeleteAmount = args[0];
         if (msgDeleteAmount) {
             if (parseInt(msgDeleteAmount)) {
-                message.channel.bulkDelete(parseInt(msgDeleteAmount))
-                    .then(deletedMessages => message.channel.send(`Deleted ${deletedMessages.size} messages`));
+                message.channel.bulkDelete(parseInt(msgDeleteAmount, true))
+                    .then(deletedMessages => message.channel.send(`Deleted ${deletedMessages.size} messages`))
+                    .catch(err => console.log(err));
             } else {
                 message.reply('enter a integer number');
             }
         } else {
-            message.channel.bulkDelete(100)
-                .then(deletedMessages => message.channel.send(`Deleted ${deletedMessages.size} messages`));
+            message.channel.bulkDelete(100, true)
+                .then(deletedMessages => message.channel.send(`Deleted ${deletedMessages.size} messages`))
+                .catch(err => console.log(err));
         }
     },
 };
